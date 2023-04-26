2f0b42911de66e50f6d56d262576b1f1e8ea1944
Mirahy <mirahyfonseca@ufgd.edu.br>
2023-04-26 15:53:18 -0400

    incluindo envio de cron para email

--- docker/nginx/init.sh
+++ docker/nginx/init.sh
@@ -3,7 +3,7 @@ apt  update --quiet
 apt  install -y --quiet cron nano wget
 # Configurando cron
 service cron start
-crontab -l | echo '*/59 * * * * /bin/sh /var/www/storage/app/script/gera-logs.sh' | crontab -
+crontab -l | echo -e 'MAILTO="ti.ead@ufgd.edu.br"\n0 2,12 * * * /bin/sh /var/www/storage/app/script/gera-logs.sh' | crontab -
 # Configurando localtime
 rm -rf /etc/localtime
 ln -s /usr/share/zoneinfo/America/Campo_Grande /etc/localtime
