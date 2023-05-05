3f89dc6aa24d96c4535c8a702545db2408ddb470
Mirahy <myrahy@gmail.com>
2023-05-05 12:52:34 -0400

    Alteração/inclusão de novos icones da biblioteca bootstrap icons

--- resources/views/config/geral.blade.php
+++ resources/views/config/geral.blade.php
@@ -7,23 +7,23 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Configurações</div>
                 @if(isset($aviso))
-                    <div style="margin: 15px;" class="alert alert-success" role="alert">{{$aviso}}</div>
+                <div style="margin: 15px;" class="alert alert-success" role="alert">{{$aviso}}</div>
                 @endif
                 <form class="form" action="" method="POST" style="text-align: left; width: 500px; margin: 15px auto;">
                     <label class="style1">E-mail Suporte:</label><br>
-                    <input class="form-control" type="text" name="email-suporte" placeholder="Ex.: suporte@gmail.com" value="{{ $emailSuporte }}" required >
-                    
+                    <input class="form-control" type="text" name="email-suporte" placeholder="Ex.: suporte@gmail.com" value="{{ $emailSuporte }}" required>
+
                     <br><label class="style1" title="Deixe vazio para utilizar o sufixo do período letivo;
 Digite 'NULL' para forçar deixar em branco.">
-                    Sufixo padrão para os Nomes das Salas (?) </label><br>
+                        Sufixo padrão para os Nomes das Salas (?) </label><br>
                     <input class="form-control" title="Deixe vazio para utilizar o sufixo do período letivo;
 Digite 'NULL' para forçar deixar em branco." type="text" name="sufixo-nome-sala" value="{{ $sufixoNomeSala }}">
 
                     <br><label class="style1">Período Letivo Padrão</label><br>
-                    <select  class="form-control" name="periodo-letivo-padrao" id="periodo-letivo-padrao">
+                    <select class="form-control" name="periodo-letivo-padrao" id="periodo-letivo-padrao">
                         <option value=""> -- nulo (obrigar seleção) -- </option>
                         @foreach ($periodoLetivos as $periodoLetivo)
-                            <option value="{{$periodoLetivo->id}}" {{$idPeriodoLetivoPadrao == $periodoLetivo->id ? 'selected' : ''}}>{{$periodoLetivo->nome . ($periodoLetivo->descricao ? " (" . $periodoLetivo->descricao . ")" : "")}}</option>
+                        <option value="{{$periodoLetivo->id}}" {{$idPeriodoLetivoPadrao == $periodoLetivo->id ? 'selected' : ''}}>{{$periodoLetivo->nome . ($periodoLetivo->descricao ? " (" . $periodoLetivo->descricao . ")" : "")}}</option>
                         @endforeach
                     </select>
 
@@ -31,18 +31,24 @@ Digite 'NULL' para forçar deixar em branco." type="text" name="sufixo-nome-sala
                     <input class="form-control" type="text" name="arquivo-saida" placeholder="Ex. backup.tar.gz" value="{{ $arquivoSaida }}" required>
 
                     <br><label class="style1">Super Macro Padrão</label><br>
-                    <select  class="form-control" name="super-macro-padrao" id="super-macro-padrao">
+                    <select class="form-control" name="super-macro-padrao" id="super-macro-padrao">
                         @foreach ($superMacros as $superMacro)
-                            <option value="{{$superMacro->id}}" {{$idSuperMacroPadrao == $superMacro->id ? 'selected' : ''}}>{{$superMacro->id. ' - ' . $superMacro->descricao}}</option>
+                        <option value="{{$superMacro->id}}" {{$idSuperMacroPadrao == $superMacro->id ? 'selected' : ''}}>{{$superMacro->id. ' - ' . $superMacro->descricao}}</option>
                         @endforeach
                     </select>
-                    
+
                     <br><label class="style1">Filtro de Emails de Usuários Liberados</label><br>
                     <input class="form-control" type="text" name="regex-liberados" value="{{ $regexLiberados }}">
 
                     <br />
-                    <input class="btn btn-primary" type="submit" name="Enviar">
-                    <button type="button" name="retrun" class="btn btn-secondary botao-barra" onclick="history.back()">Voltar</button>
+                    <button type="submit" class="btn btn-primary botao-barra" title="Enviar" name="Enviar">
+                        <span class="span-icon-button"><i class="bi bi-send"></i></span>
+                        Enviar
+                    </button>
+                    <a type="button" name="retrun" class="btn btn-secondary botao-barra" href="/">
+                        <span class="span-icon-button"><i class="bi bi-box-arrow-left"></i></span>
+                        Voltar
+                    </a>
                 </form>
             </div>
         </div>
