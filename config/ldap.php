378ef79db17adf75e9e620a4a10342d96cb47732
Mirahy <myrahy@gmail.com>
2023-05-11 08:46:25 -0400

    revert

--- config/ldap.php
+++ config/ldap.php
@@ -234,10 +234,6 @@ return [
                 'use_ssl' => env('LDAP_USE_SSL', false),
                 'use_tls' => env('LDAP_USE_TLS', false),
 
-                'custom_options' => [
-                    // See: http://php.net/ldap_set_option
-                    LDAP_OPT_X_TLS_REQUIRE_CERT => LDAP_OPT_X_TLS_NEVER
-                ]
 
             ],
 
